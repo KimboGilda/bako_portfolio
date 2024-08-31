477d9751659a72e3cbb067680c39e89c2bcc7ae2
nikos bakogiannis <nbakojohn13@gmail.com>
2024-08-31 10:16:11 +0200

    fix issue with icon open class

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let toggleMenu = () => {
   const menu = document.querySelector(".menu-links");
-  console.log(menu);
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
